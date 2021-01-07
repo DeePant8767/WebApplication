9a8c502fda9f33dd6954ba8d28c17ee6ec4f7290
Deeksha Pant <76728120+DeePant8767@users.noreply.github.com>
2021-01-07 09:50:03 +0530

    Add files via upload

---
+++ Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.NewFolder
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string nickName { get; set; }
+        public string place { get; set; }
+        public DateTime dateCreated { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ ContactController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.NewFolder;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private List<Contact> contacts = new List<Contact>
+        {
+            new Contact{Id = 1101, firstName = "Sheldon", lastName = "Cooper", nickName = "Bazzinga", place ="Texas"}
+        };
+        // GET: api/<ContactController>
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+        }
+
+        // GET api/<ContactController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Contact> Get(int id)
+        {
+
+            Contact contact = contacts.FirstOrDefault(c => c.Id == id);
+                if(contact ==  null)
+            {
+                return NotFound(new { message = "Contact Not Found." });
+            }
+            return Ok(contact);
+            
+        }
+
+        // POST api/<ContactController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ContactController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ContactController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
